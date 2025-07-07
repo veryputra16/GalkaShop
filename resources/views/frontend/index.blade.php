927c9be6aa7329a4e93cd27836932143d7761cf6
Very Putra <veryputra28@gmail.com>
2025-07-07 22:52:01 +0800

    add carousel banners for video

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,34 +3,47 @@
 @section('main-content')
 <!-- Slider Area -->
 @if(count($banners)>0)
-    <section id="Gslider" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            @foreach($banners as $key=>$banner)
-        <li data-target="#Gslider" data-slide-to="{{$key}}" class="{{(($key==0)? 'active' : '')}}"></li>
-            @endforeach
+<section id="Gslider" class="carousel slide" data-ride="carousel" data-interval="9000">
+    <ol class="carousel-indicators">
+        @foreach($banners as $key=>$banner)
+        <li data-target="#Gslider" data-slide-to="{{$key}}" class="{{ $key == 0 ? 'active' : '' }}"></li>
+        @endforeach
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        @foreach($banners as $key=>$banner)
+        <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+            @php
+                $isVideo = Str::endsWith($banner->photo, '.mp4');
+            @endphp
 
-        </ol>
-        <div class="carousel-inner" role="listbox">
-                @foreach($banners as $key=>$banner)
-                <div class="carousel-item {{(($key==0)? 'active' : '')}}">
-                    <img class="first-slide" src="{{$banner->photo}}" alt="First slide">
-                    <div class="carousel-caption d-none d-md-block text-left">
-                        <h1 class="wow fadeInDown">{{$banner->title}}</h1>
-                        <p>{!! html_entity_decode($banner->description) !!}</p>
-                        <a class="btn btn-lg ws-btn wow fadeInUpBig" href="{{route('product-grids')}}" role="button">Shop Now<i class="far fa-arrow-alt-circle-right"></i></i></a>
-                    </div>
-                </div>
-            @endforeach
+            @if($isVideo)
+                <video class="first-slide w-100" autoplay muted loop playsinline>
+                    <source src="{{ asset($banner->photo) }}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            @else
+                <img class="first-slide w-100" src="{{ asset($banner->photo) }}" alt="Banner image">
+            @endif
+
+            <div class="carousel-caption d-none d-md-block text-left">
+                <h1 class="wow fadeInDown">{{ $banner->title }}</h1>
+                <p>{!! html_entity_decode($banner->description) !!}</p>
+                <a class="btn btn-lg ws-btn wow fadeInUpBig" href="{{ route('product-grids') }}" role="button">
+                    Shop Now <i class="far fa-arrow-alt-circle-right"></i>
+                </a>
+            </div>
         </div>
-        <a class="carousel-control-prev" href="#Gslider" role="button" data-slide="prev">
+        @endforeach
+    </div>
+    <a class="carousel-control-prev" href="#Gslider" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#Gslider" role="button" data-slide="next">
+    </a>
+    <a class="carousel-control-next" href="#Gslider" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-        </a>
-    </section>
+    </a>
+</section>
 @endif
 
 <!--/ End Slider Area -->
